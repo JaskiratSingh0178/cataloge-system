58131cf3c35dc9e6fa7925466a274e172d766daf
JaskiratSingh0178 <jaskiratthind0178@gmail.com>
2026-03-09 12:48:24 +0530

    Refactor server.js to simplify API setup
    Removed MongoDB connection logic and JSON parsing middleware.

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Catalog API Running");
 });
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 
